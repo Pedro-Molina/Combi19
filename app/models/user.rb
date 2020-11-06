c27b593dd9007a9aa3732848a6a78cf0b6df7f41
Evaristo00 <evaristogaston@gmail.com>
2020-11-06 15:32:47 -0300

    agregar condiciones de validacion al crear un usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nombre, presence:true
+  validates :apellido, presence:true
+  validates :dni, presence:true, uniqueness:true, length: {minimum: 8, maximum: 8}, numericality: true
+  validates :tarjeta, presence:true, length: {minimum: 16, maximum: 16}
+
+
 end
