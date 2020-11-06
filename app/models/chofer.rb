909e9a783baa474eb81f381017f72563f824dff6
Evaristo00 <evaristogaston@gmail.com>
2020-11-06 16:05:11 -0300

    validaciones del chofer

--- app/models/chofer.rb
+++ app/models/chofer.rb
@@ -3,4 +3,10 @@ class Chofer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :validatable #borre  :registerable, para que no se puedan registrar como choferes
+
+  validates :nombre, presence:true
+  validates :apellido, presence:true
+  validates :dni, presence:true, uniqueness:true, length: {minimum: 8, maximum: 8}, numericality: true
+  validates :telefono, presence:true, numericality: true
+
 end
