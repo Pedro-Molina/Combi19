e4f2ee9f12d814e2fe2819ce6b32e25b99b0e32f
Evaristo00 <evaristogaston@gmail.com>
2020-11-07 20:16:41 -0300

    gem admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get '/user', to:'users#index'
   get '/admin', to:'admins#index'
   get '/chofer', to:'chofers#index'
