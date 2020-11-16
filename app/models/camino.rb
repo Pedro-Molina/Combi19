37fbc739953d15ddfcbb35eab7c64b762ba95e38
Evaristo00 <evaristogaston@gmail.com>
2020-11-16 14:41:14 -0300

    no permite crear camino con mismo origen destino

--- app/models/camino.rb
+++ app/models/camino.rb
@@ -1,3 +1,12 @@
+class ValidarLugar < ActiveModel::Validator
+	def validate(aux)
+		byebug
+		if aux[:origen_id].eql? aux[:destino_id]
+			aux.errors.add :base, "El origen y el destino no pueden ser los mismos"
+		end
+	end
+end
+
 class Camino < ApplicationRecord
 	validates :camino, presence:true
 	has_many :viajes
@@ -5,6 +14,7 @@ class Camino < ApplicationRecord
 	belongs_to :origen, class_name: 'Lugar'
 	belongs_to :destino, class_name: 'Lugar'
 	
+	validates_with ValidarLugar,field: [:origen, :destino]
 	
 	def custom_label_method
 		"#{self.camino}"
