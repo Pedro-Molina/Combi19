97acd75cc3f11fe5e79add614337b4849ac90be7
Tomas Pi Puig <tpipuig@gmail.com>
2020-11-16 21:50:31 -0300

    redireccion privada

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,14 +1,18 @@
 class MainController < ApplicationController
+  before_action :redireccionar
+  
+  def redireccionar
+      if user_signed_in?
+        redirect_to user_path
+      end
+      if chofer_signed_in?
+        redirect_to chofer_path
+      end
+      if admin_signed_in?
+        redirect_to  rails_admin_path
+      end
+  end
+
   def default
-  # @isLogged = user_signed_in?
-  #  if user_signed_in?
-  #    redirect_to user_path
-  #  end
-  #  if chofer_signed_in?
-  #    redirect_to chofer_path
-  #  end
-    if admin_signed_in?
-      redirect_to  rails_admin_path
-    end
   end
 end
