93f7ce51b8f1f7e28a9147b6664983f80ab64f15
Evaristo00 <evaristogaston@gmail.com>
2020-11-25 15:16:09 -0300

    formatoViajeDestino

--- app/models/viaje.rb
+++ app/models/viaje.rb
@@ -13,18 +13,8 @@ class Viaje < ApplicationRecord
     
     #accepts_nested_attributes_for :camino
 
-    def self.search(search)
-
- 		if search 
-
-    		where('title LIKE ?', "%#{search}%")
-
-  		else
-
-    		scoped
-
-  		end
-
-	end
+    def formatoViaje
+        "#{Lugar.formatoViajeOrigen} - #{Lugar.formatoViajeDestino} - #{Viaje.FechayHora}" # Origen Destino Fecha
+    end
 
 end
