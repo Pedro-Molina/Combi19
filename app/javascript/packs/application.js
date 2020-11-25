f6118c572ea5ac608b3e14bea65cc215acf1cc6f
Tomas Pi Puig <tpipuig@gmail.com>
2020-11-25 17:31:32 -0300

    select 2 y jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+//= require("select2-rails")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
