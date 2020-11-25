4153d2dae57da1b5014096a31a219c268a37c7b7
Tomas Pi Puig <tpipuig@gmail.com>
2020-11-25 19:09:20 -0300

    no anda viaje controller

--- app/controllers/viajes_controller.rb
+++ app/controllers/viajes_controller.rb
@@ -1,6 +1,6 @@
 class ViajesController < ApplicationController
 	def index
-
+    @viajeSolo = Viaje.first
 		@viaje = Viaje.all
 
   		#@viaje = Vaije.search(params[:camino])
@@ -8,4 +8,15 @@ class ViajesController < ApplicationController
       	#format.html # index.html.erb
 
     end
+
+		def update
+      byebug
+	    @persona=Viaje.find(params[:viaje])
+
+	    if @persona.update(users:)
+	      redirect_to root_path
+	    else
+	      render :edit
+	    end
+	  end
 end
--- app/models/viaje.rb
+++ app/models/viaje.rb
@@ -1,20 +1,18 @@
 class Viaje < ApplicationRecord
-    
-    #has_and_belongs_to_many :users
+
+    has_and_belongs_to_many :users
     belongs_to :combi
     belongs_to :chofer
     belongs_to :camino
 
-    validates :chofer_id, uniqueness: { scope: [:camino_id, :FechayHora, :combi_id] }#, message: "must be given please" 
+    validates :chofer_id, uniqueness: { scope: [:camino_id, :FechayHora, :combi_id] }#, message: "must be given please"
 
     def custom_label_method
 		"Viaje #{self.id}"
     end
-    
-    #accepts_nested_attributes_for :camino
+
 
     def formatoViaje
-        #"#{Lugar.formatoViajeOrigen(self)} - #{Lugar.formatoViajeDestino(self)} - #{Viaje.FechayHora}" # Origen Destino Fecha
         "#{Lugar.find(Camino.find(self.camino_id).origen_id).ciudad}/#{Lugar.find(Camino.find(self.camino_id).origen_id).provincia} - #{Lugar.find(Camino.find(self.camino_id).destino_id).ciudad}/#{Lugar.find(Camino.find(self.camino_id).destino_id).provincia} - #{self.FechayHora.to_formatted_s(:db)}" # Origen Destino Fecha
     end
 
