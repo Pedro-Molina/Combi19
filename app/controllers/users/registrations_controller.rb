ab40cf2f5f9854fc48fd46205b2c94c794ffed41
Tomas Pi Puig <tpipuig@gmail.com>
2020-12-01 22:14:14 -0300

    listo para demo 2

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,6 +4,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
    before_action :configure_sign_up_params, only: [:create]
    before_action :configure_account_update_params, only: [:update]
 
+
+
   # GET /resource/sign_up
    def new
      super
@@ -21,6 +23,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # PUT /resource
    def update
+     #byebug
+     #params.require(:user).permit(:nombre, :apellido, :dni, :tarjeta, :FechaExpiracion)
      super
    end
 
@@ -47,9 +51,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+   def configure_account_update_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:nombre, :apellido, :dni, :tarjeta, :FechaExpiracion])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:nombre, :apellido, :dni, :tarjeta, :FechaExpiracion])
+     #devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+   end
 
   # The path used after sign up.
    def after_sign_up_path_for(resource)
@@ -59,5 +65,25 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # The path used after sign up for inactive accounts.
    def after_inactive_sign_up_path_for(resource)
      super(resource)
+   end
+
+   protected
+
+   def update_resource(resource, params)
+     parametros=params
+     #esto es para que no aparezcan notificaciones
+     if parametros[:current_password]&.present?
+       pass=parametros[:current_password]
+     else
+      pass="evagay123444"
+     end
+     parametros[:password]=pass
+     parametros[:password_confirmation]=pass
+     return super
+     # Require current password if user is trying to change password.
+     #return super if params["password"]&.present?
+
+     # Allows user to update registration information without password.
+     #resource.update_without_password(parametros)
    end
 end
